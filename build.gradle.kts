1602cd5ff8a26583845b9fd9acd8e2e6f94843bd
dingyong07 <dingyong07@meituan.com>
2026-06-29 10:36:56 +0800

    更新脚本解决编译问题

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,8 +3,8 @@
 // ============================================================
 
 plugins {
-    id("com.android.application").version("8.2.2").apply(false)
-    id("com.android.library").version("8.2.2").apply(false)
+    id("com.android.application").version("8.6.1").apply(false)
+    id("com.android.library").version("8.6.1").apply(false)
     kotlin("android").version("2.1.21").apply(false)
     kotlin("multiplatform").version("2.1.21").apply(false)
     id("com.google.devtools.ksp").version("2.1.21-2.0.1").apply(false)
@@ -23,6 +23,10 @@ buildscript {
 }
 
 allprojects {
+    if (version == Project.DEFAULT_VERSION) {
+        version = "1.0.0"
+    }
+
     repositories {
         google()
         mavenCentral()
